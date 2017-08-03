49425e187679bbc93e1bba8b0a5c01d60da47964
Ruben L <ruben.labiunda@openmailbox.org>
2017-08-03 16:27:28 +0200

    adds custom metalsmith demo plugin

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var Metalsmith  = require('./lib');
 var sass        = require('metalsmith-sass');
 var concat      = require('metalsmith-concat');
+var markdown    = require('metalsmith-markdown');
+var layouts     = require('metalsmith-layouts');
+const example   = require('./modules/example');
 
 Metalsmith(__dirname)
+  .metadata({
+    title: "My Static Site & Blog whäääää",
+    description: "It's about saying »Hello« to the World whääää.",
+    generator: "Metalsmith",
+    url: "http://www.metalsmith.io/"
+  })
   .source('./src')
   .destination('./build')
-  .clean(false)
+  .ignore([
+    '.*.*',  //ignore hidden files like .eslintrc
+    //'layouts' //ignore layouts folder when not prebuild pages
+  ])
+  .clean(true)
   .use(sass({
     sourceMap: true,
     sourceMapContents: true,
@@ -17,10 +30,21 @@ Metalsmith(__dirname)
   .use(concat({
     //files: 'js/**/*.js',
     files: [
+      '../node_modules/riot/riot.min.js',
+      //'tags/*.tag.html',
       'js/setup.js',
-      'asdf.js'
+      'js/asdf.js'
     ],
     output: 'assets/main.js',
+  }))
+  .use(example({
+    but: 'whole'
+  }))
+  //.use(listRiotTags({butt: 'hole'}))
+  .use(markdown())
+  .use(layouts({
+    engine: 'handlebars',
+    directory: './src/layouts'
   }))
   .build(function(err) {
     if (err) { throw err; }
