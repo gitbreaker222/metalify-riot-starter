7c6427925e4772d158d0a11f231b3352395e60f3
Ruben L <ruben.labiunda@openmailbox.org>
2017-08-08 04:09:40 +0200

    removes semicolons

--- modules/example.js
+++ modules/example.js
@@ -1,5 +1,5 @@
 // Include debug to help with debugging.
-var debug = require('debug')('metalsmith-name-of-my-plugin');
+var debug = require('debug')('metalsmith-name-of-my-plugin')
 
 var metalsmith_plugin = function (opts) {
 
@@ -9,30 +9,30 @@ var metalsmith_plugin = function (opts) {
     return function (files, metalsmith, done) {
 
         // Metalsmith metadata usage:
-        var metadata = metalsmith.metadata();
+        var metadata = metalsmith.metadata()
 
         // Loop through files
         Object.keys(files).forEach(function(file){
-            debug('checking file: %s', file);
-            //var file_data = files[file];
+            debug('checking file: %s', file)
+            //var file_data = files[file]
             console.log('##', file)
         })
 
         // Errors should be reported if necessary
         /*
         if (has_issue) {
-            done(new Error('Explain the issue'));
+            done(new Error('Explain the issue'))
         }
         */
 
         console.log('It works :) This is your metadata:', metadata)
-        if (opts) console.log('…and these are the given options', opts);
+        if (opts) console.log('…and these are the given options', opts)
 
         // Call done() to tell Metalsmith it has finished.
         // This allows us to work asynchronously and call it when we are done.
-        done();
-    };
-};
+        done()
+    }
+}
 
 // Expose the plugin
-module.exports = metalsmith_plugin;
+module.exports = metalsmith_plugin
