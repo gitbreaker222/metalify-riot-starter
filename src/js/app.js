56f593a687f8ecd957292da393f05fb0d48a0e1c
Ruben L <ruben.labiunda@openmailbox.org>
2017-09-11 20:33:16 +0200

    routs first level of pages + perfect loading screen

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,30 @@
 APP SETUP
 */
 var app = {}
+app.data = {}
+app.services = {}
+app.ctrl = {}
 riot.observable(app)
 
-// define event names
+// define event names, convention
 app.evt = {
+  init: 'init',
+  appReady: 'appReade',
+  contentLoaded: 'contentLoaded',
   contentReady: 'contentReady',
   error: 'error'
 }
+
+//init
+app.one(app.evt.init, function () {
+  riot.mount('*')
+})
+app.one(app.evt.appReady, function () {
+  //load content
+  var url = '/content.json'
+  app.one(app.evt.contentLoaded, function (data) {
+    app.data.content = JSON.parse(data)
+    app.trigger(app.evt.contentReady)
+  })
+  app.load(url, app.evt.contentLoaded)
+})
--- src/js/controller/init-controller.js
+++
@@ -1,8 +0,0 @@
-app.init = function () {
-  riot.mount('*')
-  var url = '/content.json'
-  app.one(app.evt.contentReady, function (data) {
-    app.content = JSON.parse(data)
-  })
-  app.load(url, app.evt.contentReady)
-}
